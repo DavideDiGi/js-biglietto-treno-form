4d87904c294df6f781529a95bdbb32eb204dbd34
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-20 16:04:13 +0100

    fine prima card

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let kilometers = prompt("Quanti chilometri devi percorrere?");
+// let age = prompt("Quanti anni hai?");
+
+// // console.log("chilometri: " + kilometers);
+// // console.log("anni: " + age);
+
+// let ticketPrice;
+
+// ticketPrice = 0.21 * kilometers;
+
+// // console.log("prezzo del biglietto: " + ticketPrice +"€");
+
+
+// let youngerPrice;
+// let olderPrice;
+
+// if (age < 18) {
+//     youngerPrice = (ticketPrice - (ticketPrice * 20) / 100);
+
+//     youngerPrice = youngerPrice.toFixed(2);
+
+//     document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto giovani: " + youngerPrice + "€" ;
+
+//     // console.log("prezzo scontato : " + youngerPrice + "€");
+
+// }
+
+// else if (age >= 65) {
+//     olderPrice = (ticketPrice - (ticketPrice * 40) / 100);
+
+//     olderPrice = olderPrice.toFixed(2);
+
+//     document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto pensionati: " + olderPrice + "€" ;
+
+//     // console.log("prezzo scontato : " + olderPrice + "€");
+// }
+
+// else{
+//     document.getElementById("final-price").innerHTML = "prezzo del biglietto: " + ticketPrice + "€" ;
+// }
+
