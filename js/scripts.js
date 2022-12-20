abee0164df225b84d17571d0d11c8d87ed932abd
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-20 17:40:41 +0100

    inizio javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,34 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // let kilometers = prompt("Quanti chilometri devi percorrere?");
 // let age = prompt("Quanti anni hai?");
 
-// // console.log("chilometri: " + kilometers);
-// // console.log("anni: " + age);
+// console.log("chilometri: " + kilometers);
+// console.log("anni: " + age);
 
-// let ticketPrice;
+let nameInput = document.getElementById("name-input");
+let kmInput = document.getElementById("km-input");
+let ageSelect = document.getElementById("age-select");
+let createButton = document.getElementById("create-button");
+let deleteButton = document.getElementById("delete-button");
+
+let ticketPrice;
+
+
+createButton.addEventListener('click',
+
+    function() {
+        console.log("cliccato su Genera")
+        console.log("Il nome dell'utente è:", nameInput.value);
+        console.log("I km da percorrere sono:", kmInput.value);
+        console.log("Il conducente è:", ageSelect);
+    }
+
+)
+
 
 // ticketPrice = 0.21 * kilometers;
 
-// // console.log("prezzo del biglietto: " + ticketPrice +"€");
+// console.log("prezzo del biglietto: " + ticketPrice +"€");
 
 
 // let youngerPrice;
@@ -55,9 +39,9 @@
 
 //     youngerPrice = youngerPrice.toFixed(2);
 
-//     document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto giovani: " + youngerPrice + "€" ;
+    // document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto giovani: " + youngerPrice + "€" ;
 
-//     // console.log("prezzo scontato : " + youngerPrice + "€");
+//     console.log("prezzo scontato : " + youngerPrice + "€");
 
 // }
 
@@ -66,12 +50,49 @@
 
 //     olderPrice = olderPrice.toFixed(2);
 
-//     document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto pensionati: " + olderPrice + "€" ;
+//     // document.getElementById("final-price").innerHTML = "prezzo del biglietto con sconto pensionati: " + olderPrice + "€" ;
 
-//     // console.log("prezzo scontato : " + olderPrice + "€");
+//     console.log("prezzo scontato : " + olderPrice + "€");
 // }
 
 // else{
-//     document.getElementById("final-price").innerHTML = "prezzo del biglietto: " + ticketPrice + "€" ;
+//     // document.getElementById("final-price").innerHTML = "prezzo del biglietto: " + ticketPrice + "€" ;
+
+//     console.log("prezzo del biglietto: " + ticketPrice + "€");
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
